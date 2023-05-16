5a4591c3b6a9403e0a20769b19847af5debf91a6
Nageswara Mannem <nageswara@syftdata.com>
2023-05-15 17:05:34 -0700

    syft init

--- syft/events.ts
+++ syft/events.ts
@@ -2,14 +2,67 @@
 import type { type, SyftEventType } from '@syftdata/client';
 
 /**
- * test
- * @type {SyftEventType.SCREEN}
+ * This event tracks page views in the application. Log the event when a page is rendered.
+ * @type {SyftEventType.PAGE}
  */
-class UserCreated {
+export class PageViewed {
   /**
-   * Documentation is not available.
-   * Please add it in the model file.
+   * Name of the page.
    */
-  userid: string;
+  name: string;
+
+  /**
+   * Path portion of the page’s URL. Equivalent to location.pathname from the DOM API.
+   */
+  path?: string;
+
+  /**
+   * Previous page’s full URL. Equivalent to document.referrer from the DOM API.
+   */
+  referrer?: string;
+
+  /**
+   * Query string portion of the page’s URL. Equivalent to location.search from the DOM API.
+   */
+  search?: string;
+
+  /**
+   * Page’s title. Equivalent to document.title from the DOM API.
+   */
+  title?: string;
+
+  /**
+   * Page’s full URL.
+   */
+  url?: string;
+
+}
+
+/**
+ * Emit this event to identify a Group. Analytics Plugins can make `.set_group` calls when this event is seen.
+ * @type {SyftEventType.GROUP_IDENTIFY}
+ */
+export class GroupIdentity {
+  /**
+   * Type of the group. e.g: age-bucket, organization.
+   */
+  groupType: string;
+
+  /**
+   * Id of the group. e.g. "18-24", "syft-org"
+   */
+  groupId: string;
+
+}
+
+/**
+ * Emit this event to identify a User. Analytics Plugins can make `.identify` calls when this event is seen.
+ * @type {SyftEventType.IDENTIFY}
+ */
+export class UserIdentity {
+  /**
+   * Id of the user.
+   */
+  userId: type.UUID;
 
 }
